e13f393b32640acfe39a96baa62472cf5090b85b
AllisonMarieLee <119071092+AllisonMarieLee@users.noreply.github.com>
2023-02-08 20:12:53 -0600

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,17 @@
+fun main() {
+   var name="Allison Lee"
+    var age=34
+    var annualPay:Double=100000.00
+    
+    println("My name is $name, my age is $age and \nI hope to earn $$annualPay per year. \n")
+    
+    
+    //or like this:
+    print("My name is ");
+    print(name);
+    print(", my age is ");
+    print(age);
+    print(" and \nI hope to earn $");
+    print(annualPay)
+    print(" per year.")
+}
