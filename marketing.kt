cbe24415f7d69f84e0c4be0264142e71c8b66a44
AllisonMarieLee <119071092+AllisonMarieLee@users.noreply.github.com>
2023-02-08 19:49:28 -0600

    Create marketing.kt

---
+++ marketing.kt
@@ -0,0 +1,9 @@
+fun main() {
+   var friendsCount: Int = 500
+    
+    if (friendsCount >= 500)
+    println("Adopt a Dog Today!")
+    
+    else if (friendsCount < 500)
+    println("Buy Cat Food At 20% Off")
+}
