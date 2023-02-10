0aae7713f594d9755e6a4bc360a0df2caa80acae
AllisonMarieLee <119071092+AllisonMarieLee@users.noreply.github.com>
2023-02-09 20:27:34 -0600

    Create salesPrediction.kt

---
+++ salesPrediction.kt
@@ -0,0 +1,6 @@
+fun main() {
+    var eastCoastPercentage: Double=0.62
+    var companySales= 4600000	
+    val prediction= eastCoastPercentage * companySales
+    println("The East Coast sales division is predicted to generate $$prediction this year.")
+}
