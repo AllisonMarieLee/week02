c0c4558f3fa0be34ded6f34fd1a461a918f3a389
AllisonMarieLee <119071092+AllisonMarieLee@users.noreply.github.com>
2023-02-11 11:58:53 -0600

    Create stringMani.kt

---
+++ stringMani.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var faveCity= "Honolulu"
+    println("Enter the name of your favorite city: $faveCity") // user input
+    
+    
+    // The number of characters in the city name
+    println(faveCity.length) // output is 8 
+           
+    // The name of the city in all uppercase letters
+   	println(faveCity.uppercase()) 
+     
+    // The name of the city in all lowercase letters
+    println(faveCity.lowercase())    
+     
+    // The first character in the name of the city
+    print(faveCity[0]) // first element (H)
+     
+}
